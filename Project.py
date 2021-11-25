be52f2d4070fd28ac709a9401c966e8d8b18ba6f
JordanCSmart <49327082+JordanCSmart@users.noreply.github.com>
2021-11-25 16:36:40 -0700

    Add files via upload

--- Project.py
+++ Project.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[13]:
-
-
 import numpy as np
 import math as math
 import matplotlib.pyplot as plt
 from scipy import special
 
 from mpl_toolkits import mplot3d
-
-
-# In[27]:
-
 
 def NewtonSystem(f, g, h, J, x0, y0, z0, max, tol):
     X0 = np.array([[x0], [y0],[z0]])
@@ -25,37 +18,40 @@ def NewtonSystem(f, g, h, J, x0, y0, z0, max, tol):
             return X1
         X0 = X1
     raise ValueError('not enought iterations')
-def ffun(x,y,z):
-    return  x - np.log(10-y*x)
 
-def gfun(x,y,z):
-    return y - np.log(10-z*y)
-
-def hfun(x,y,z):
-    return z - np.log(10-z*x)
-
-def J(x,y,z):
-    return np.array([[1-(1/(1-y*x))*(y),0,-(1/(1-z*x))*z], [-(1/(1-y*x))*x,1-(1/(1-z*y))*x,0],[0,-(1/(1-z*y))*y,1-(1/(1-z*x))*x]])
+def BFGS(f, g, h, J, x0, y0, z0, max, tol):
+    X0 = np.array([x0, y0, z0])
+    X1 = np.array([0, 0,0])
+    grad = np.gradient([f(x0, y0, z0), g(x0, y0, z0), h(x0, y0, z0)])
+    B0 = np.transpose(J(grad[0], grad[1], grad[2]))
+    for i in range(0, max):
+        grad = np.gradient([f(X0[0], X0[1], X0[2]), g(X0[0], X0[1], X0[2]), h(X0[0], X0[1], X0[2])])
+        pk = np.linalg.inv(B0) @ -grad
+        alpha = 10000
+        for j in range(1, 101):
+            if alpha > np.argmin(f(X0[0] + j*pk[0], X0[1] + j*pk[1], X0[2] + j*pk[2])):
+                alpha = np.argmin(f(X0[0] + j*pk[0], X0[1] + j*pk[1], X0[2] + j*pk[2]))
+        sk = alpha*pk
+        X1 = X0 + sk
+        yk = np.gradient([f(X1[0], X1[1], X1[2]), g(X1[0], X1[1], X1[2]), h(X1[0], X1[1], X0[2])]) - grad
+        X0 = X1
+        B0 = B0 + ((yk-B0@sk)@np.transpose((yk-B0@sk)))/(np.transpose(sk)@(yk-B0))
+    return X0
 
 print('newtons:')
-answer = NewtonSystem(ffun, gfun, hfun,J, 3,3,3, 100000, 1e-3)
+answer = NewtonSystem(lambda x, y, z: x - np.log(10-y*x), lambda x, y, z: y - np.log(10-z*y), lambda x, y, z: z - np.log(10-z*x), lambda x, y, z: np.array([[1-(1/(1-y*x))*(y),0,-(1/(1-z*x))*z], [-(1/(1-y*x))*x,1-(1/(1-z*y))*x,0],[0,-(1/(1-z*y))*y,1-(1/(1-z*x))*x]]), 3,3,3, 100000, 1e-3)
 print('x =', answer[0][0])
 print('y =', answer[1][0])
 print('z =', answer[2][0])
 
-ax = plt.axes(projection = '3d')
+# ax = plt.axes(projection = '3d')
 
-dim = np.linspace(0,3,1000)
+# dim = np.linspace(0,3,1000)
 
-xline = ffun(dim,dim,dim)
-yline = ffun(dim,dim,dim)
-zline = ffun(dim,dim,dim)
-ax.scatter3D(answer[0][0],answer[1][0],answer[2][0])
-ax.plot3D(xline,yline,zline,'grey')
+# xline = lambda x, y, z: x - np.log(10-y*x)(dim,dim,dim)
+# yline = lambda x, y, z: x - np.log(10-y*x)(dim,dim,dim)
+# zline = lambda x, y, z: x - np.log(10-y*x)(dim,dim,dim)
+# ax.scatter3D(answer[0][0],answer[1][0],answer[2][0])
+# ax.plot3D(xline,yline,zline,'grey')
 
-
-# In[ ]:
-
-
-
-
+print(BFGS(lambda x, y, z: x - np.log(10-y*x), lambda x, y, z: y - np.log(10-z*y), lambda x, y, z: z - np.log(10-z*x), lambda x, y, z: np.array([[1-(1/(1-y*x))*(y),0,-(1/(1-z*x))*z], [-(1/(1-y*x))*x,1-(1/(1-z*y))*x,0],[0,-(1/(1-z*y))*y,1-(1/(1-z*x))*x]]), 3, 3, 3, 100, 1e-3))
