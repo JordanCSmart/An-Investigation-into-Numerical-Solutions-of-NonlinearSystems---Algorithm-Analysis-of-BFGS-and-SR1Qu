7ec0037c7b721514e6b754c1fe1b8e76f84e1dc1
JordanCSmart <49327082+JordanCSmart@users.noreply.github.com>
2021-11-30 18:13:27 -0700

    Update Project.py

--- Project.py
+++ Project.py
@@ -9,11 +9,9 @@ import numdifftools as nd
 
 from mpl_toolkits import mplot3d
 
-def line_search(f, X0, pk):
-    alpha = 1
-    grad = nd.Gradient(f)(X0)
-    Grad = np.transpose(grad)
-    while f(X0- alpha*pk) < f(X0) + 0.1*alpha*Grad@pk:
+def line_search(f, grad, X0, pk):
+    alpha = 100
+    while f(X0- alpha*pk) <= f(X0) + 0.1*alpha*grad@pk:
         alpha = alpha * 0.1
                                                      
     return alpha
@@ -23,7 +21,8 @@ def NewtonSystems(f, B, X0, max, tol):
     for i in range(0, max):
         grad = nd.Gradient(f)(X0)
         pk = np.linalg.inv(B(X0)) @ -grad
-        alpha = line_search(f, X0, pk)
+        alpha = line_search(f, grad, X0, pk)
+        print(alpha)
         sk = alpha*pk
         X1 = X0 + sk
         for j in range(0, len(X0)):
@@ -37,8 +36,8 @@ def BFGS(f, B, X0, max, tol):
     B0 = B(X0)
     for i in range(0, max):
         grad = nd.Gradient(f)(X0)
-        pk = np.linalg.inv(B(X0)) @ -grad
-        alpha = line_search(f, X0, pk)
+        pk = np.linalg.inv(B0) @ -grad
+        alpha = line_search(f, grad, X0, pk)
         sk = alpha*pk
         X1 = X0 + sk
         yk = nd.Gradient(f)(X1) - grad
@@ -46,7 +45,25 @@ def BFGS(f, B, X0, max, tol):
             if abs(X0[j]-X1[j]) > tol: break
             elif j == len(X0)-1: return X0
         X0 = X1
-        B0 = B0 - (B0@sk@np.transpose(sk)*B0)/(np.transpose(sk)@B0@sk) + (yk@np.transpose(yk))/(np.transpose(yk)@sk)
+        B0 = B0 - ((yk-B0@sk)@np.transpose(yk-B0@sk))/(np.transpose(sk)@(yk-B0@sk))
+    return 'not enough iterations'
+
+def SR1(f, B, X0, max, tol):
+    X1 = 0
+    B0 = B(X0)
+    H0 = np.linalg.inv(B0)
+    for i in range(0, max):
+        grad = nd.Gradient(f)(X0)
+        pk = -H0 @ grad
+        alpha = line_search(f, grad, X0, pk)
+        sk = alpha*pk
+        X1 = X0 + sk
+        yk = nd.Gradient(f)(X1) - grad
+        for j in range(0, len(X0)):
+            if abs(X0[j]-X1[j]) > tol: break
+            elif j == len(X0)-1: return X0
+        X0 = X1
+        H0 = H0 +((sk-H0@yk)@np.transpose(sk-H0@yk))/(np.transpose(yk)@(sk-H0@yk))
     return 'not enough iterations'
 
 # ax = plt.axes(projection = '3d')
@@ -65,5 +82,6 @@ def func(x):
 def H(x):
     return np.array([[-400*(x[1]-x[0]**2)+800*x[0]**2+2, -400*x[0]], [-400*x[0], 200]])
 
-print(NewtonSystems(func, H, [1, 0], 100, 1e-3))
-print(BFGS(func, H, [1, 0], 100, 1e-3))
+print(NewtonSystems(func, H, [10, 10], 100, 1e-3))
+print(BFGS(func, H, [10, 10], 100, 1e-3))
+print(SR1(func, H, [10, 10], 100, 1e-3))
