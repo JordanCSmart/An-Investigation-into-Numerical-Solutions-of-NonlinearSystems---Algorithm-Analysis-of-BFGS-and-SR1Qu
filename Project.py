2fb667708e1bafc594940f6a749b7e58194e8510
JordanCSmart <49327082+JordanCSmart@users.noreply.github.com>
2021-12-02 18:49:49 -0700

    Update Project.py

--- Project.py
+++ Project.py
@@ -10,67 +10,74 @@ import numdifftools as nd
 from mpl_toolkits import mplot3d
 
 def line_search(f, X0, pk, max):
-    alpha = 1.0
+    alpha = 10
     it = 0
-    while f(X0+ alpha*pk) <= (f(X0) + 0.55*alpha*np.transpose(nd.Gradient(f)(X0))@pk) :
+    while f(X0+ alpha*pk) <= (f(X0) + alpha*np.transpose(nd.Gradient(f)(X0))@pk) :
         alpha = alpha * 0.1
         it = it +1
-        print(alpha)
-        if it == max:
+        if it == max or alpha == 0:
             break
                                            
     return alpha
 
+def gradient(x):
+    return np.array([-2*(1-x[0])**2-40*x[0]*(x[1]-x[0]**2), 20*(x[1]-x[0]**2)])
+
 def NewtonSystems(f, B, X0, max, tol):
     X1 = 0
     for i in range(0, max):
+        if i == 1: print(B(X0))
         grad = nd.Gradient(f)(X0)
         pk = np.linalg.inv(B(X0)) @ -grad
-        alpha = line_search(f, X0, pk, max)
-        #print(alpha)
+        alpha = optimize.line_search(f, gradient, X0, pk)
+        alpha = alpha[0]
         sk = alpha*pk
         X1 = X0 + sk
         for j in range(0, len(X0)):
             if abs(X0[j]-X1[j]) > tol: break
-            elif j == len(X0)-1: return X0
+            elif j == len(X0)-1: return X1
         X0 = X1
     return X0
 
 def BFGS(f, B, X0, max, tol):
     X1 = 0
-    B0 = B(X0)
+    B0 = np.linalg.inv(B(X0))
     for i in range(0, max):
+        if i == 1: print(np.linalg.inv(B0))
         grad = nd.Gradient(f)(X0)
-        pk = np.linalg.inv(B0) @ -grad
-        alpha = line_search(f, X0, pk, max)
-        print(alpha)
+        pk = B0 @ -grad
+        alpha = optimize.line_search(f, gradient, X0, pk)
+        alpha = alpha[0]
         sk = alpha*pk
         X1 = X0 + sk
         yk = nd.Gradient(f)(X1) - grad
         for j in range(0, len(X0)):
             if abs(X0[j]-X1[j]) > tol: break
-            elif j == len(X0)-1: return X0
+            elif j == len(X0)-1: return X1
         X0 = X1
-        B0 = B0 - ((yk-B0@sk)@np.transpose(yk-B0@sk))/(np.transpose(sk)@(yk-B0@sk))
+        pk = np.transpose(np.array([pk]))
+        sk = np.transpose(np.array([sk]))
+        yk = np.transpose(np.array([yk]))
+        B0 = B0 + ((np.transpose(sk)@yk+np.transpose(yk)@B0@yk)*(sk@np.transpose(sk))/(np.transpose(sk)@yk)**2) - ((B0@yk@np.transpose(sk)+sk@np.transpose(yk)@B0))/(np.transpose(sk)@yk)
     return 'not enough iterations'
 
-def SR1(f, B, X0, max, tol):
-    X1 = 0
-    B0 = B(X0)
-    H0 = np.linalg.inv(B0)
-    for i in range(0, max):
-        grad = nd.Gradient(f)(X0)
-        pk = -H0 @ grad
-        alpha = line_search(f, X0, pk, max)
-        sk = alpha*pk
-        X1 = X0 + sk
-        yk = nd.Gradient(f)(X1) - grad
-        for j in range(0, len(X0)):
-            if abs(X0[j]-X1[j]) > tol: break
-            elif j == len(X0)-1: return X0
-        X0 = X1
-        H0 = H0 +((sk-H0@yk)@np.transpose(sk-H0@yk))/(np.transpose(yk)@(sk-H0@yk))
-    return 'not enough iterations'
+# def SR1(f, B, X0, max, tol):
+#     X1 = 0
+#     B0 = B(X0)
+#     H0 = np.linalg.inv(B0)
+#     for i in range(0, max):
+#         grad = nd.Gradient(f)(X0)
+#         pk = -H0 @ grad
+#         alpha = line_search(f, X0, pk, max)
+#         sk = alpha*pk
+#         X1 = X0 + sk
+#         yk = nd.Gradient(f)(X1) - grad
+#         for j in range(0, len(X0)):
+#             if abs(X0[j]-X1[j]) > tol: break
+#             elif j == len(X0)-1: return X0
+#         X0 = X1
+#         H0 = H0 +((sk-H0@yk)@np.transpose(sk-H0@yk))/(np.transpose(yk)@(sk-H0@yk))
+#     return 'not enough iterations'
 
 # ax = plt.axes(projection = '3d')
 
@@ -88,9 +95,6 @@ def func(x):
 def H(x):
     return np.array([[-400*(x[1]-x[0]**2)+800*x[0]**2+2, -400*x[0]], [-400*x[0], 200]])
 
-print('hello')
-
-print(NewtonSystems(func, H, [1.5, 1.5], 100000, 1e-4))
-print('here')
-print(BFGS(func, H, [1.5, 1.5], 100000, 1e-4))
-print(SR1(func, H, [1.5, 1.5], 100000, 1e-4))
+print(NewtonSystems(func, H, [-2, 4], 100000, 1e-4))
+print(BFGS(func, H, [-2, 4], 100000, 1e-4))
+#print(SR1(func, H, [1.5, 1.5], 100000, 1e-4))
