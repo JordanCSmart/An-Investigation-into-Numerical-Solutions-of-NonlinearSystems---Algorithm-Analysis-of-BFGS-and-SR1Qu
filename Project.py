8c17fe567b6d58566ab8adae9cbb8c210d477435
JordanCSmart <49327082+JordanCSmart@users.noreply.github.com>
2021-12-04 09:12:32 -0700

    Update Project.py

--- Project.py
+++ Project.py
@@ -17,11 +17,10 @@ warnings.filterwarnings('ignore')
 def NewtonSystems(f, B, gradient, X0, max, tol):
     X1 = 0
     for i in range(0, max):
-        grad = gradient(X0)
-        pk = np.linalg.inv(B(X0)) @ -grad
+        pk = np.linalg.inv(B(X0)) @ -gradient(X0)
         alpha = optimize.line_search(f, gradient, X0, pk)
-        if type(alpha[0]) != type(None): alpha = alpha[0]
-        else: alpha = 1
+        if type(alpha[0]) != type(None): alpha = alpha[0]*.3
+        else: alpha = alpha[4]*.3
         sk = alpha*pk
         X1 = X0 + sk
         for j in range(0, len(X0)):
@@ -36,8 +35,8 @@ def BFGS(f, B, gradient, X0, max, tol):
     for i in range(0, max):
         pk = B0 @ -gradient(X0)
         alpha = optimize.line_search(f, gradient, X0, pk)
-        if type(alpha[0]) != type(None): alpha = alpha[0]
-        else: alpha = 1
+        if type(alpha[0]) != type(None): alpha = alpha[0]*.3
+        else: alpha = 1*.3
         sk = alpha*pk
         X1 = X0 + sk
         yk = gradient(X1)-gradient(X0)
@@ -58,8 +57,8 @@ def SR1(f, B, gradient, X0, max, tol):
     for i in range(0, max):
         pk = B0 @ -gradient(X0)
         alpha = optimize.line_search(f, gradient, X0, pk)
-        if type(alpha[0]) != type(None): alpha = alpha[0]
-        else: alpha = 1
+        if type(alpha[0]) != type(None): alpha = alpha[0]*.3
+        else: alpha = 1*.3
         sk = alpha*pk
         X1 = X0 + sk
         yk = gradient(X1)-gradient(X0)
@@ -90,54 +89,238 @@ hr1 = 0
 hr2 = 0
 hr3 = 0
 
-def func(x):
-    return (1-x[0])**2+100*(x[1]-x[0]**2)**2
+# def func(x):
+#     return (1-x[0])**2+100*(x[1]-x[0]**2)**2
 
-def H(x):
-    return np.array([[-400*(x[1]-x[0]**2)+800*x[0]**2+2, -400*x[0]], [-400*x[0], 200]])
+# def H(x):
+#     return np.array([[-400*(x[1]-x[0]**2)+800*x[0]**2+2, -400*x[0]], [-400*x[0], 200]])
 
-def gradient1(x):
-    return np.array([-2*(1-x[0])**2-400*x[0]*(x[1]-x[0]**2), 200*(x[1]-x[0]**2)])
+# def gradient1(x):
+#     return np.array([-2*(1-x[0])**2-400*x[0]*(x[1]-x[0]**2), 200*(x[1]-x[0]**2)])
 
-s = time.time()
-a1 = NewtonSystems(func, H, gradient1, [-1, 20], 100000, 1e-4)
-e = time.time()
-hr1 = e-s
-s = time.time()
-a2 = BFGS(func, H, gradient1, [-1, 20], 100000, 1e-4)
-e = time.time()
-hr2 = e-s
-s = time.time()
-a3 = SR1(func, H, gradient1, [-1, 20], 100000, 1e-4)
-e = time.time()
-hr3 = e-s
-it1 = a1[1]
-it2 = a2[1]
-it3 = a3[1]
-print("Newtons Method", "number of iterations:", a1[1], ", time taken:", hr1, "secends")
-print("BFGS", "number of iterations:", a2[1], ", time taken:", hr2, "secends")
-print("SR1", "number of iterations:", a3[1], ", time taken:", hr3, "secends")
-print("newtons results", a1[0])
-print("BFGS results", a1[0])
+# s = time.time()
+# a1 = NewtonSystems(func, H, gradient1, [-2, 20], 100000, 1e-4)
+# e = time.time()
+# hr1 = e-s
+# s = time.time()
+# a2 = BFGS(func, H, gradient1, [-2, 20], 100000, 1e-4)
+# e = time.time()
+# hr2 = e-s
+# s = time.time()
+# a3 = SR1(func, H, gradient1, [-2, 20], 100000, 1e-4)
+# e = time.time()
+# hr3 = e-s
+# it1 = a1[1]
+# it2 = a2[1]
+# it3 = a3[1]
+# print("Newtons Method", "number of iterations:", a1[1], ", time taken:", hr1, "secends")
+# print("BFGS", "number of iterations:", a2[1], ", time taken:", hr2, "secends")
+# print("SR1", "number of iterations:", a3[1], ", time taken:", hr3, "secends")
+# print("Newton values", a1[0])
+# print("BFGS values", a2[0])
+# print("SR1 values", a3[0])
 
-# def func2(x):
-#     y = 0
-#     for i in range(0, len(x)):
-#         y = y + x[i]**2
-#     return y
+def func2(x):
+    y = 0
+    for i in range(0, len(x)):
+        y = y + x[i]**2
+    return y
 
-# def H2(x):
-#     return 2*np.identity(len(x))
+def H2(x):
+    return 2*np.identity(len(x))
 
-# def gradient2(x):
-#     y = []
-#     for i in range(0, len(x)):
-#         y.append(2*x[i])
-#     return np.array(y)
+def gradient2(x):
+    y = []
+    for i in range(0, len(x)):
+        y.append(2*x[i])
+    return np.array(y)
 
-# input = []
-# for j in range(0, 50000):
-#    input.append(j+1)
+input = []
+for j in range(0, 5):
+   input.append(j+1)
+
+iterationsnewtons = []
+timearraynewtons = []
+iterationbfgs = []
+timearraybfgs = []
+iterationssr1 = []
+timearraysr1 = []
+
+ts = time.time()
+a = NewtonSystems(func2, H2, gradient2, input, 10000000000, 1e-4)
+te = time.time()
+iterationsnewtons.append(5)
+timearraynewtons.append(te-ts)
+ts = time.time()
+a = BFGS(func2, H2, gradient2, input, 10000000000, 1e-4)
+te = time.time()
+iterationbfgs.append(5)
+timearraybfgs.append(te-ts)
+ts = time.time()
+a = SR1(func2, H2, gradient2, input, 10000000000, 1e-4)
+te = time.time()
+iterationssr1.append(5)
+timearraysr1.append(te-ts)
+
+for j in range(0, 10):
+   input.append(j+1)
+
+ts = time.time()
+a = NewtonSystems(func2, H2, gradient2, input, 10000000000, 1e-4)
+te = time.time()
+iterationsnewtons.append(10)
+timearraynewtons.append(te-ts)
+ts = time.time()
+a = BFGS(func2, H2, gradient2, input, 10000000000, 1e-4)
+te = time.time()
+iterationbfgs.append(10)
+timearraybfgs.append(te-ts)
+ts = time.time()
+a = SR1(func2, H2, gradient2, input, 10000000000, 1e-4)
+te = time.time()
+iterationssr1.append(10)
+timearraysr1.append(te-ts)
+
+for j in range(0, 50):
+   input.append(j+1)
+
+ts = time.time()
+a = NewtonSystems(func2, H2, gradient2, input, 10000000000, 1e-4)
+te = time.time()
+iterationsnewtons.append(50)
+timearraynewtons.append(te-ts)
+ts = time.time()
+a = BFGS(func2, H2, gradient2, input, 10000000000, 1e-4)
+te = time.time()
+iterationbfgs.append(50)
+timearraybfgs.append(te-ts)
+ts = time.time()
+a = SR1(func2, H2, gradient2, input, 10000000000, 1e-4)
+te = time.time()
+iterationssr1.append(50)
+timearraysr1.append(te-ts)
+
+for j in range(0, 100):
+   input.append(j+1)
+
+ts = time.time()
+a = NewtonSystems(func2, H2, gradient2, input, 10000000000, 1e-4)
+te = time.time()
+iterationsnewtons.append(100)
+timearraynewtons.append(te-ts)
+ts = time.time()
+a = BFGS(func2, H2, gradient2, input, 10000000000, 1e-4)
+te = time.time()
+iterationbfgs.append(100)
+timearraybfgs.append(te-ts)
+ts = time.time()
+a = SR1(func2, H2, gradient2, input, 10000000000, 1e-4)
+te = time.time()
+iterationssr1.append(100)
+timearraysr1.append(te-ts)
+
+for j in range(0, 500):
+   input.append(j+1)
+
+ts = time.time()
+a = NewtonSystems(func2, H2, gradient2, input, 10000000000, 1e-4)
+te = time.time()
+iterationsnewtons.append(500)
+timearraynewtons.append(te-ts)
+ts = time.time()
+a = BFGS(func2, H2, gradient2, input, 10000000000, 1e-4)
+te = time.time()
+iterationbfgs.append(500)
+timearraybfgs.append(te-ts)
+ts = time.time()
+a = SR1(func2, H2, gradient2, input, 10000000000, 1e-4)
+te = time.time()
+iterationssr1.append(500)
+timearraysr1.append(te-ts)
+
+for j in range(0, 1000):
+   input.append(j+1)
+
+ts = time.time()
+a = NewtonSystems(func2, H2, gradient2, input, 10000000000, 1e-4)
+te = time.time()
+iterationsnewtons.append(a[1])
+timearraynewtons.append(te-ts)
+ts = time.time()
+a = BFGS(func2, H2, gradient2, input, 10000000000, 1e-4)
+te = time.time()
+iterationbfgs.append(a[1])
+timearraybfgs.append(te-ts)
+ts = time.time()
+a = SR1(func2, H2, gradient2, input, 10000000000, 1e-4)
+te = time.time()
+iterationssr1.append(a[1])
+timearraysr1.append(te-ts)
+
+for j in range(0, 5000):
+   input.append(j+1)
+
+ts = time.time()
+a = NewtonSystems(func2, H2, gradient2, input, 10000000000, 1e-4)
+te = time.time()
+iterationsnewtons.append(a[1])
+timearraynewtons.append(te-ts)
+ts = time.time()
+a = BFGS(func2, H2, gradient2, input, 10000000000, 1e-4)
+te = time.time()
+iterationbfgs.append(a[1])
+timearraybfgs.append(te-ts)
+ts = time.time()
+a = SR1(func2, H2, gradient2, input, 10000000000, 1e-4)
+te = time.time()
+iterationssr1.append(a[1])
+timearraysr1.append(te-ts)
+
+for j in range(0, 10000):
+   input.append(j+1)
+
+ts = time.time()
+a = NewtonSystems(func2, H2, gradient2, input, 10000000000, 1e-4)
+te = time.time()
+iterationsnewtons.append(a[1])
+timearraynewtons.append(te-ts)
+ts = time.time()
+a = BFGS(func2, H2, gradient2, input, 10000000000, 1e-4)
+te = time.time()
+iterationbfgs.append(a[1])
+timearraybfgs.append(te-ts)
+ts = time.time()
+a = SR1(func2, H2, gradient2, input, 10000000000, 1e-4)
+te = time.time()
+iterationssr1.append(a[1])
+timearraysr1.append(te-ts)
+
+for j in range(0, 50000):
+   input.append(j+1)
+
+ts = time.time()
+a = NewtonSystems(func2, H2, gradient2, input, 10000000000, 1e-4)
+te = time.time()
+iterationsnewtons.append(a[1])
+timearraynewtons.append(te-ts)
+ts = time.time()
+a = BFGS(func2, H2, gradient2, input, 10000000000, 1e-4)
+te = time.time()
+iterationbfgs.append(a[1])
+timearraybfgs.append(te-ts)
+ts = time.time()
+a = SR1(func2, H2, gradient2, input, 10000000000, 1e-4)
+te = time.time()
+iterationssr1.append(a[1])
+timearraysr1.append(te-ts)
+
+plt.loglog(iterationsnewtons, timearraynewtons, label='newtons')
+plt.loglog(iterationbfgs, timearraybfgs, label='bfgs')
+plt.loglog(iterationssr1, timearraysr1, label='sr1')
+plt.xlabel('size of n')
+plt.ylabel('time in seconds')
+plt.legend()
+plt.show()
 
 # ts1 = time.time()
 # a1 = NewtonSystems(func2, H2, gradient2, input, 10000000000, 1e-4)
@@ -189,8 +372,9 @@ print("BFGS results", a1[0])
 # print("BFGS", "number of iterations:", a2[1], ", time taken:", hr2, "secends")
 # hr3 = hr3 + (te3 - ts3)
 # print("SR1", "number of iterations:", a3[1], ", time taken:", hr3, "secends")
-# print("newtons results", a1[0])
-# print("BFGS results", a1[0])
+# print("Newton values", a1[0])
+# print("BFGS values", a2[0])
+# print("SR1 values", a3[0])
 
 # def func4(x):
 #     x1 = x[0]
@@ -225,8 +409,11 @@ print("BFGS results", a1[0])
 # print("BFGS", "number of iterations:", a2[1], ", time taken:", hr2, "secends")
 # hr3 = hr3 + (te3 - ts3)
 # print("SR1", "number of iterations:", a3[1], ", time taken:", hr3, "secends")
+# print("Newton values", a1[0])
+# print("BFGS values", a2[0])
+# print("SR1 values", a3[0])
 
-print("Newtons Method", it1, "iterations,", hr1, "seconds",  hr1/it1, "seconds per iteration")
-print("BFGS", it2, "iterations,", hr2, "seconds", hr2/it2, "seconds per iteration")
-print("SR1", it3, "iterations,", hr3, "seconds", hr3/it3, "seconds per iteration")
-print("\nBFGS is", hr1/hr2, "times faster than newtons and", hr1/it1/(hr2/it2), "per iteration faster")
+# print("\nNewtons Method", it1, "iterations,", hr1, "seconds",  hr1/it1, "seconds per iteration")
+# print("BFGS", it2, "iterations,", hr2, "seconds", hr2/it2, "seconds per iteration")
+# print("SR1", it3, "iterations,", hr3, "seconds", hr3/it3, "seconds per iteration")
+# print("\nBFGS is", hr1/hr2, "times faster than newtons and", hr1/it1/(hr2/it2), "per iteration faster")
